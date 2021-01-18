c9060eec78b4750b7e708333e87ea1f42950ef74
Paul Nieuwenhuis <paul.nieuwenhuis@newhouse-it.nl>
2021-01-18 19:01:38 +0100

    Bring back original files, make other customized

--- images/linux/scripts/SoftwareReport/SoftwareReport.Generator.Customized.ps1
+++ images/linux/scripts/SoftwareReport/SoftwareReport.Generator.Customized.ps1
@@ -33,28 +33,28 @@ $markdown += New-MDHeader "Language and Runtime" -Level 3
 
 $markdown += New-MDList -Style Unordered -Lines @(
         (Get-CPPVersions),
-        (Get-FortranVersions),
+        #(Get-FortranVersions),
         (Get-ClangVersions),
-        (Get-ErlangVersion),
-        (Get-MonoVersion),
-        (Get-NodeVersion),
+        #(Get-ErlangVersion),
+        # (Get-MonoVersion),
+        # (Get-NodeVersion),
         (Get-PythonVersion),
         (Get-Python3Version),
         (Get-PowershellVersion),
-        (Get-RubyVersion),
-        (Get-SwiftVersion),
-        (Get-JuliaVersion)
+        (Get-RubyVersion)
+        #(Get-SwiftVersion),
+        #(Get-JuliaVersion)
 )
 
 $markdown += New-MDHeader "Package Management" -Level 3
 
 $packageManagementList = @(
-        (Get-HomebrewVersion),
-        (Get-GemVersion),
-        (Get-MinicondaVersion),
+        #(Get-HomebrewVersion),
+        #(Get-GemVersion),
+        # (Get-MinicondaVersion),
         (Get-HelmVersion),
-        (Get-NpmVersion),
-        (Get-YarnVersion),
+        # (Get-NpmVersion),
+        # (Get-YarnVersion),
         (Get-PipVersion),
         (Get-Pip3Version),
         (Get-VcpkgVersion)
@@ -68,23 +68,23 @@ if (-not (Test-IsUbuntu16)) {
 
 $markdown += New-MDList -Style Unordered -Lines ($packageManagementList | Sort-Object)
 
-$markdown += New-MDHeader "Project Management" -Level 3
-$markdown += New-MDList -Style Unordered -Lines @(
-        (Get-AntVersion),
-        (Get-GradleVersion),
-        (Get-MavenVersion),
-        (Get-SbtVersion)
-)
+# $markdown += New-MDHeader "Project Management" -Level 3
+# $markdown += New-MDList -Style Unordered -Lines @(
+#         (Get-AntVersion),
+#         (Get-GradleVersion),
+#         (Get-MavenVersion),
+#         (Get-SbtVersion)
+# )
 
 $markdown += New-MDHeader "Tools" -Level 3
 $toolsList = @(
     (Get-7zipVersion),
-    (Get-AnsibleVersion),
+    #(Get-AnsibleVersion),
     (Get-AptFastVersion),
     (Get-AzCopy7Version),
     (Get-AzCopy10Version),
-    (Get-BazelVersion),
-    (Get-BazeliskVersion),
+    #(Get-BazelVersion),
+    #(Get-BazeliskVersion),
     (Get-CodeQLBundleVersion),
     (Get-CMakeVersion),
     (Get-CurlVersion),
@@ -94,27 +94,27 @@ $toolsList = @(
     (Get-GitVersion),
     (Get-GitLFSVersion),
     (Get-GitFTPVersion),
-    (Get-GoogleCloudSDKVersion),
-    (Get-HavegedVersion),
-    (Get-HerokuVersion),
-    (Get-HHVMVersion),
-    (Get-SVNVersion),
+    #(Get-GoogleCloudSDKVersion),
+    # (Get-HavegedVersion),
+    #(Get-HerokuVersion),
+    #(Get-HHVMVersion),
+    #(Get-SVNVersion),
     (Get-JqVersion),
     (Get-KindVersion),
     (Get-KubectlVersion),
-    (Get-KustomizeVersion),
-    (Get-LeiningenVersion),
-    (Get-M4Version),
-    (Get-HGVersion),
+    #(Get-KustomizeVersion),
+    #(Get-LeiningenVersion),
+    #(Get-M4Version),
+    #(Get-HGVersion),
     (Get-MinikubeVersion),
-    (Get-NewmanVersion),
-    (Get-NvmVersion),
+    # (Get-NewmanVersion),
+    # (Get-NvmVersion),
     (Get-PackerVersion),
-    (Get-PhantomJSVersion),
-    (Get-PulumiVersion),
-    (Get-RVersion),
-    (Get-SphinxVersion),
-    (Get-SwigVersion),
+    #(Get-PhantomJSVersion),
+    #(Get-PulumiVersion),
+    #(Get-RVersion),
+    #(Get-SphinxVersion),
+    #(Get-SwigVersion),
     (Get-TerraformVersion),
     (Get-UnZipVersion),
     (Get-WgetVersion),
@@ -135,60 +135,60 @@ $markdown += New-MDList -Style Unordered -Lines ($toolsList | Sort-Object)
 
 $markdown += New-MDHeader "CLI Tools" -Level 3
 $markdown += New-MDList -Style Unordered -Lines @(
-    (Get-AlibabaCloudCliVersion),
-    (Get-AWSCliVersion),
-    (Get-AWSCliSessionManagerPluginVersion),
-    (Get-AWSSAMVersion),
+    #(Get-AlibabaCloudCliVersion),
+    #(Get-AWSCliVersion),
+    #(Get-AWSCliSessionManagerPluginVersion),
+    #(Get-AWSSAMVersion),
     (Get-AzureCliVersion),
     (Get-AzureDevopsVersion),
-    (Get-GitHubCliVersion),
-    (Get-HubCliVersion),
-    (Get-NetlifyCliVersion),
-    (Get-OCCliVersion),
-    (Get-ORASCliVersion),
-    (Get-VerselCliversion)
+    (Get-GitHubCliVersion)
+    #(Get-HubCliVersion),
+    #(Get-NetlifyCliVersion),
+    #(Get-OCCliVersion),
+    #(Get-ORASCliVersion),
+    #(Get-VerselCliversion)
 )
 
-$markdown += New-MDHeader "Java" -Level 3
-$markdown += Get-JavaVersions | New-MDTable
-$markdown += New-MDNewLine
+# $markdown += New-MDHeader "Java" -Level 3
+# $markdown += Get-JavaVersions | New-MDTable
+# $markdown += New-MDNewLine
 
-$markdown += New-MDHeader "PHP" -Level 3
-$markdown += Build-PHPTable | New-MDTable
-$markdown += New-MDNewLine
+# $markdown += New-MDHeader "PHP" -Level 3
+# $markdown += Build-PHPTable | New-MDTable
+# $markdown += New-MDNewLine
 
-$markdown += New-MDHeader "Haskell" -Level 3
-$markdown += New-MDList -Style Unordered -Lines @(
-    (Get-GHCVersion),
-    (Get-CabalVersion),
-    (Get-StackVersion)
-)
+# $markdown += New-MDHeader "Haskell" -Level 3
+# $markdown += New-MDList -Style Unordered -Lines @(
+#     (Get-GHCVersion),
+#     (Get-CabalVersion),
+#     (Get-StackVersion)
+# )
 
-$markdown += New-MDHeader "Rust Tools" -Level 3
-$markdown += New-MDList -Style Unordered -Lines @(
-    (Get-RustVersion),
-    (Get-RustupVersion),
-    (Get-RustdocVersion),
-    (Get-CargoVersion)
-)
+# $markdown += New-MDHeader "Rust Tools" -Level 3
+# $markdown += New-MDList -Style Unordered -Lines @(
+#     (Get-RustVersion),
+#     (Get-RustupVersion),
+#     (Get-RustdocVersion),
+#     (Get-CargoVersion)
+# )
 
-$markdown += New-MDHeader "Packages" -Level 4
-$markdown += New-MDList -Style Unordered -Lines @(
-    (Get-BindgenVersion),
-    (Get-CargoAuditVersion),
-    (Get-CargoOutdatedVersion),
-    (Get-CargoClippyVersion),
-    (Get-CbindgenVersion),
-    (Get-RustfmtVersion)
-)
+# $markdown += New-MDHeader "Packages" -Level 4
+# $markdown += New-MDList -Style Unordered -Lines @(
+#     (Get-BindgenVersion),
+#     (Get-CargoAuditVersion),
+#     (Get-CargoOutdatedVersion),
+#     (Get-CargoClippyVersion),
+#     (Get-CbindgenVersion),
+#     (Get-RustfmtVersion)
+# )
 
-$markdown += New-MDHeader "Browsers and Drivers" -Level 3
-$markdown += New-MDList -Style Unordered -Lines @(
-    (Get-ChromeVersion),
-    (Get-ChromeDriverVersion),
-    (Get-FirefoxVersion),
-    (Get-GeckodriverVersion)
-)
+# $markdown += New-MDHeader "Browsers and Drivers" -Level 3
+# $markdown += New-MDList -Style Unordered -Lines @(
+#     (Get-ChromeVersion),
+#     (Get-ChromeDriverVersion),
+#     (Get-FirefoxVersion),
+#     (Get-GeckodriverVersion)
+# )
 
 $markdown += New-MDHeader ".NET Core SDK" -Level 3
 $markdown += New-MDList -Style Unordered -Lines @(
@@ -200,22 +200,22 @@ $markdown += New-MDList -Style Unordered -Lines @(
     (Get-AzModuleVersions)
 )
 
-$markdown += New-MDHeader "Databases" -Level 3
-$markdown += New-MDList -Style Unordered -Lines @(
-    (Get-PostgreSqlVersion),
-    (Get-MongoDbVersion),
-    (Get-SqliteVersion)
-)
+# $markdown += New-MDHeader "Databases" -Level 3
+# $markdown += New-MDList -Style Unordered -Lines @(
+#     (Get-PostgreSqlVersion),
+#     (Get-MongoDbVersion),
+#     (Get-SqliteVersion)
+# )
 
-$markdown += Build-MySQLSection
-$markdown += Build-MSSQLToolsSection
+# $markdown += Build-MySQLSection
+# $markdown += Build-MSSQLToolsSection
 
-$markdown += New-MDHeader "Cached Tools" -Level 3
-$markdown += Build-CachedToolsSection
+# $markdown += New-MDHeader "Cached Tools" -Level 3
+# $markdown += Build-CachedToolsSection
 
-$markdown += New-MDHeader "Android" -Level 3
-$markdown += Build-AndroidTable | New-MDTable
-$markdown += New-MDNewLine
+# $markdown += New-MDHeader "Android" -Level 3
+# $markdown += Build-AndroidTable | New-MDTable
+# $markdown += New-MDNewLine
 
 $markdown += New-MDHeader "Cached Docker images" -Level 3
 $markdown += Get-CachedDockerImagesTableData | New-MDTable
